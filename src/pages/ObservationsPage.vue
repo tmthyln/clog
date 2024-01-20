6948c580304bef3f65f7bf2317562fa228a204f2
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-19 23:56:59 -0500

    feat: content for observations page

--- src/pages/ObservationsPage.vue
+++ src/pages/ObservationsPage.vue
@@ -1,9 +1,69 @@
 <script setup lang="ts">
+import {useCatStore} from "../stores/cats.ts";
+import {ref} from "vue";
+import {objectDate} from "../stores/dates.ts";
 
+const catStore = useCatStore();
+
+const dateObserved = ref<string>('');
+const observation = ref<string>('');
+
+function addObservation() {
+    if (catStore.currentCat) {
+        catStore.addObservation(catStore.currentCat.id, observation.value, dateObserved.value)
+    }
+}
 </script>
 
 <template>
+  <div class="m-4">
+    <h1 class="title is-2">
+      About {{ catStore.currentCat?.name || 'a Cat' }}
+    </h1>
 
+    <div v-if="catStore.cats.length === 0">
+      You don't have any cats yet!
+      <router-link :to="{name: 'cats'}">Add some cats first</router-link>, then come back.
+    </div>
+    <div v-else-if="!catStore.currentCat">
+      Choose a cat from your clowder from the cat switcher in the menu.
+    </div>
+    <div v-else>
+      <div>
+        <div class="field">
+          <label class="label">Date observed</label>
+
+          <div class="control">
+            <input class="input" type="date" :value="dateObserved" @input="dateObserved = $event.target.value">
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Observation or event</label>
+
+          <textarea class="textarea is-expanded" :value="observation" @input="observation = $event.target.value"/>
+        </div>
+
+        <div class="control">
+          <button class="button is-primary" @click="addObservation">Record</button>
+        </div>
+      </div>
+
+      <div class="panel mt-5">
+        <div class="panel-block" v-for="observation in catStore.observations">
+          <div class="columns">
+            <small class="column is-narrow">
+              On {{ objectDate(observation.observedDate).toLocaleDateString() }},
+            </small>
+            <div class="column">
+              {{ observation.notes }}
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
