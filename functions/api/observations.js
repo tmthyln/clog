42f39058df559b6fc69b21f1b7f8319b388a70ae
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-19 23:56:21 -0500

    feat: return observations sorted by recency

--- functions/api/observations.js
+++ functions/api/observations.js
@@ -12,7 +12,7 @@ function convertDatabaseObservation(data) {
 export async function onRequestGet(context) {
     // TODO filter based on selected cat
     const { results: observations } = await context.env.DB
-        .prepare('SELECT id, logged_date, observed_date, notes, cat_id FROM observation')
+        .prepare('SELECT id, logged_date, observed_date, notes, cat_id FROM observation ORDER BY observed_date DESC')
         .all();
 
     return Response.json(observations.map(convertDatabaseObservation));
