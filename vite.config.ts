5ca654edd8e45adf05a5af2cc7b74a4741a0af57
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-20 20:27:45 -0500

    feat: add compression for assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
     plugins: [
         vue(),
+        viteCompression({
+            algorithm: 'gzip',
+        }),
+        viteCompression({
+            algorithm: 'brotliCompress',
+            ext: 'br',
+        }),
     ],
 })
