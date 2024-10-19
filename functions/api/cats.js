6c207f1c0bc72fbac06add88f4d79666d1022c7b
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-10-18 20:51:26 -0400

    chore: light refactor

--- functions/api/cats.js
+++ functions/api/cats.js
@@ -10,12 +10,12 @@ export async function onRequestGet(context) {
 export async function onRequestPost(context) {
     const {name, birthdate} = await context.request.json();
     const db = context.env.DB;
-    const results = await db.batch([
+    const [_, {results}] = await db.batch([
         db.prepare('INSERT INTO cat (name, birthdate) VALUES (?, ?) ON CONFLICT(name) DO UPDATE SET birthdate = ?').bind(name, birthdate, birthdate),
         db.prepare('SELECT id, name, birthdate FROM cat WHERE name = ?').bind(name),
     ]);
 
-    return Response.json(results[1].results[0])
+    return Response.json(results[0])
 }
 
 export async function onRequestDelete(context) {
